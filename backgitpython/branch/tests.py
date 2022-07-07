9c02ce3264c1685cab355732030e04665e86ea4c
ilichgithub <ilichrondon@gmail.com>
2022-07-07 09:02:36 -0400

    test

--- backgitpython/branch/tests.py
+++ backgitpython/branch/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class BranchesAPIViewTest(APITestCase):
+	def setUp(self):
+		pass
+		
+	def tearDown(self):
+		pass
+		
+	def test_get_branches(self):
+		url = reverse("branch")
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		data = response.json()
+		print(data)
+		self.assertFalse( "branchActive" in data )
+		
+	def test_get_branches(self):
+		url = reverse("branch")
+		response = self.client.get(url)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+		data = response.json()
+		print(data)
